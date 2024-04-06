3c7783a530d7ed30cd5cb1d4a2a7a323c0864a18
mathisdrn <116900975+mathisdrn@users.noreply.github.com>
2024-04-06 22:28:55 +0200

    rm conf.py

--- conf.py
+++
@@ -1,7 +0,0 @@
-nb_remove_code_source = True
-nb_remove_code_outputs = True
-nb_code_prompt_hide = False
-nb_number_source_lines = True
-nb_output_stderr = True
-nb_merge_streams = True
-myst_render_markdown_format = "myst"
